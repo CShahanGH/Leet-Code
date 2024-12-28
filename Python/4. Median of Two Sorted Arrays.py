cb77d4962b4ab4e73d3200b355a65208cac53be2
Charlie <charlieshahan@gmail.com>
2024-12-27 19:34:33 -0600

    Median of Two Sorted Arrays finished

--- Python/4. Median of Two Sorted Arrays.py
+++ Python/4. Median of Two Sorted Arrays.py
@@ -23,12 +23,12 @@ nums2.length == n
 0 <= m <= 1000
 0 <= n <= 1000
 1 <= m + n <= 2000
--106 <= nums1[i], nums2[i] <= 106
+-10*6 <= nums1[i], nums2[i] <= 10*6
 """
 
 #Idea 
 """
-See Design
+Design on github https://github.com/CShahanGH/Leet-Code/blob/main/Python/Design/Median%20of%20Two%20Sorted%20Arrays.png
 Credits to Tushar Roy https://www.youtube.com/watch?v=LPFhl65R7ww&t=369s
 """
 
@@ -39,17 +39,59 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        totalsize = len(nums1) + len(nums2)
 
-        odd = True if totalsize % 2 == 1 else False
+        #To avoid duplicate code I will ensure nums1 is always the shortest array
+        if len(nums2) < len(nums1):
+            return self.findMedianSortedArrays(nums2, nums1)
+    
+        nums1length = len(nums1)
+        nums2length = len(nums2)
+        totallength = nums1length + nums2length
+        start = 0
+        end = nums1length
+        odd = True if totallength % 2 == 1 else False
+        maxnum = 10 ** 6
+        minnum = -10 ** 6
+
+        #Find partition for first array (drop decimal)
+        nums1partition = (start + end)//2
+
+        #Find Partition for second array (drop decimal)
+        nums2partition = (totallength + 1)//2 - nums1partition
+
+        #Continue adjusting the partition until all elements of the left size are <= to all elements on the right side 
+        FOUND = False
+        while not FOUND:
+
+            #If element is out of bonds on the left side (< 0) set to minnum. If element is out of bonds on the right side (> length) set to maxnum
+            maxLeft1 = nums1[nums1partition - 1] if (nums1partition - 1) >= 0 else minnum 
+            maxLeft2 = nums2[nums2partition - 1] if (nums2partition - 1) >= 0 else minnum
+            minRight1 = nums1[nums1partition] if (nums1partition) < nums1length else maxnum
+            minRight2 = nums2[nums2partition] if (nums2partition) < nums2length else maxnum
+
+            if maxLeft1 <= minRight2 and maxLeft2 <= minRight1:
+                FOUND = True
+            elif maxLeft1 > minRight2: #Move partition towards left in nums1
+                end = nums1partition - 1
+                nums1partition = (start + end)//2
+                nums2partition = (totallength + 1)//2 - nums1partition
+            else: #Move partition towards right in nums1
+                start = nums1partition + 1
+                nums1partition = (start + end)//2
+                nums2partition = (totallength + 1)//2 - nums1partition
+            
+        if odd: 
+            return max(maxLeft1, maxLeft2)
+        else:
+            return float(max(maxLeft1, maxLeft2) + min(minRight1, minRight2))/2
+
 
         
-
 
                 
 test = Solution()
 
-nums1 = [1, 2, 3, 7, 8, 9]
-nums2 = [4, 5, 6, 10, 11, 12, 13]
+nums1 = [1, 2]
+nums2 = [3, 4]
 print(test.findMedianSortedArrays(nums1, nums2))
 
