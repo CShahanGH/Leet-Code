d96119d6e993e878be0442f806dac44c6d26fd36
Charlie <charlieshahan@gmail.com>
2025-01-02 13:30:57 -0600

    Renamed some files in design to match their problem number and started 7

---
+++ Python/7. Reverse Integer.py
@@ -0,0 +1,60 @@
+#Problem Requirements
+"""
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+"""
+
+#Problem Requirements
+
+
+
+#Design (might be in Design folder)
+
+
+
+#Start
+class Solution(object):
+    pass #Starting Code Here
+
+
+#Test Code
+def Test(program_output, expected_output):
+    try:
+        assert program_output == expected_output
+        return True
+    except AssertionError as e: 
+        return False
+
+
+solution = Solution()
+
+#Test 1
+input = None
+output = None
+answer = None
+
+if Test(output, answer):
+    print("Test 1  passed")
+else:
+    print("Test 1 failed")
