c7790bd37567a6533f4448612bd0bdce35589352
Charlie <charlieshahan@gmail.com>
2025-01-05 18:11:32 -0600

    Need help AAA

---
+++ Python/10. Regular Expression Matching.py
@@ -0,0 +1,325 @@
+#Problem Requirements
+"""
+Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+
+'.' Matches any single character.​​​​
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+ 
+
+Example 1:
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+
+Example 2:
+Input: s = "aa", p = "a*"
+Output: true
+Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+
+Example 3:
+Input: s = "ab", p = ".*"
+Output: true
+Explanation: ".*" means "zero or more (*) of any character (.)".
+ 
+
+Constraints:
+
+1 <= s.length <= 20
+1 <= p.length <= 20
+s contains only lowercase English letters.
+p contains only lowercase English letters, '.', and '*'.
+It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
+"""
+
+#Design (In Design Folder)
+"""
+You can look at my attempt in the design folder and old code, but this solution comes from Tushar Roy again
+"""
+
+#Start
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        
+
+
+
+#Old Code
+"""
+ sp = 0 #string index pointer
+        pp = 0 #pattern index pointer
+
+        while sp < len(s) and pp < len(p):
+            if p[pp] == s[sp] or p[pp] == ".": #Move both best case a = a
+                pp += 1
+                sp += 1
+            elif p[pp] != s[sp] and p[pp] != "*": # a = b 
+                #Try for Zero Star
+                pp += 1
+                if pp >= len(p): #Out of Bonds Not Match
+                    return False 
+                if p[pp] != "*":
+                    return False #No * to make zero *
+                else:
+                    pp += 1
+                    #MAYBE A BOUNDS CHECK
+            else: #pp-> *
+                starp = pp
+                
+                #CASE WHEN * is ONLY ONE OF THE PREVIOUS ELEMENT
+                if pp + 1 < len(p):
+                    if p[pp + 1] == s[sp] and s[sp] == p[pp - 1] or p[pp + 1] == ".": #RAAAAAAAAAAAA
+
+                        #RANOM CHECK for ".*.." HEHEHEHEHE meaing *. is 0
+                        if pp + 2 < len(p):
+                            if p[pp+1] == p[pp+2] or p[pp+2] == ".":
+                                pp += 2 #SKIP BOFA
+                        sp += 1
+                        pp += 1
+                        continue #Continue to top loop
+                    
+
+                #0* star loop
+                while pp <len(p):
+                    pp += 1
+                    if pp >= len(p): #Out of bounds failed 0* try next check after loop
+                        pp -= 1 #STUPID should not be s[sp]
+                        break
+                    if p[pp] != s[sp] and p[pp] != ".": #Go for *
+                        pp += 1
+                        if pp >= len(p): #Out of bounds failed 0* try next check after loop
+                            pp -= 1 #STUPID should be s[sp]
+                            break
+                        if p[pp] != "*":
+                            pp -= 1 #STUPID should be s[sp]
+                            break
+                        else: #pp->* continue zero star check
+                            continue
+                    else: #p[pp] == s[sp] break and continue top loop
+
+                        starp2 = pp
+                        #ANOTHER 0 STAR FILTER BECAUSE WHY NOT
+                        while pp < len(p):
+                            if p[pp] != "*": #Go for star
+                                pp += 1
+                                if pp >= len(p): #Out of bounds failed 0* return False for leftover pattern not matche 
+                                    pp -= 1
+                                    break
+                                if p[pp] != "*":  #Still no star match not found
+                                    pp -= 1
+                                    break
+                            else: #pp -> *
+                                pp += 1
+                        if p[pp] != p[starp2]:
+                            pp = starp2
+                        break #AAAAAAAA
+                if p[pp] == s[sp] or p[pp] == ".": 
+                    continue #Go to top loop
+                #0* loop failed try previous pp multiple times
+                pp = starp
+                pp = pp - 1 #DON'T CARE 
+                if p[pp] != s[sp] and p[pp] != ".": #Preceeding element does not match
+                    return False
+                
+
+        #CleanUp
+        if sp != len(s): #String not matched complete
+            return False 
+
+        
+        #Todo Cleanup Leftover Zero*s 
+        while pp < len(p):
+            if p[pp] != "*": #Go for *
+                pp += 1
+                if pp >= len(p): #Out of bounds failed 0* return False for leftover pattern not matche 
+                    return False 
+                if p[pp] != "*":  #Still no star match not found
+                    return False
+            if pp + 1 < len(p):
+                    pp += 1
+            else: #pp -> *
+                pp += 1
+
+        return True #If the pattern survived all the checks return true
+"""
+#Test Code
+def Test(program_output, expected_output):
+    try:
+        assert program_output == expected_output
+        return True
+    except AssertionError as e: 
+        return False
+
+
+solution = Solution()
+
+
+
+
+#Test 1
+inputstring = "aa"
+inputpattern = "a"
+output = solution.isMatch(inputstring, inputpattern)
+answer = False
+if Test(output, answer):
+    print("Test 1 passed")
+else:
+    print(f"Test 1 failed got {output} expected {answer}")
+
+"""
+#Test 2
+inputstring = "aaaaaa"
+inputpattern = "a*"
+output = solution.isMatch(inputstring, inputpattern)
+answer = True
+if Test(output, answer):
+    print("Test 2 passed")
+else:
+    print(f"Test 2 failed got {output} expected {answer}")
+
+#Test 3
+inputstring = "ab"
+inputpattern = ".*"
+output = solution.isMatch(inputstring, inputpattern)
+answer = True
+if Test(output, answer):
+    print("Test 3 passed")
+else:
+    print(f"Test 3 failed got {output} expected {answer}")
+
+#Test 4
+inputstring = "aab"
+inputpattern = "c*a*b"
+output = solution.isMatch(inputstring, inputpattern)
+answer = True
+if Test(output, answer):
+    print("Test 4 passed")
+else:
+    print(f"Test 4 failed got {output} expected {answer}")
+
+#Test 5
+inputstring = "abcd"
+inputpattern = "d*"
+output = solution.isMatch(inputstring, inputpattern)
+answer = False
+if Test(output, answer):
+    print("Test 5 passed")
+else:
+    print(f"Test 5 failed got {output} expected {answer}")
+
+#Test 6
+inputstring = "ab"
+inputpattern = ".*c"
+output = solution.isMatch(inputstring, inputpattern)
+answer = False
+if Test(output, answer):
+    print("Test 6 passed")
+else:
+    print(f"Test 6 failed got {output} expected {answer}")
+
+#Test 7
+inputstring = "aaa"
+inputpattern = "ab*a*c*a"
+output = solution.isMatch(inputstring, inputpattern)
+answer = True
+if Test(output, answer):
+    print("Test 7 passed")
+else:
+    print(f"Test 7 failed got {output} expected {answer}")
+
+#Test 8
+inputstring = "aaaaaaaaaaaaaaaab"
+inputpattern = "ab*ac*a*"
+output = solution.isMatch(inputstring, inputpattern)
+answer = False
+if Test(output, answer):
+    print("Test 8 passed")
+else:
+    print(f"Test 8 failed got {output} expected {answer}")
+
+#Test 9
+inputstring = "abc"
+inputpattern = "abcd*e*f*"
+output = solution.isMatch(inputstring, inputpattern)
+answer = True
+if Test(output, answer):
+    print("Test 9 passed")
+else:
+    print(f"Test 9 failed got {output} expected {answer}")
+
+#Test 10
+inputstring = "aaaaaa"
+inputpattern = "a*a*a*"
+output = solution.isMatch(inputstring, inputpattern)
+answer = True
+if Test(output, answer):
+    print("Test 10 passed")
+else:
+    print(f"Test 10 failed got {output} expected {answer}")
+
+#Test 11
+inputstring = "aaa"
+inputpattern = "aaaa"
+output = solution.isMatch(inputstring, inputpattern)
+answer = False
+if Test(output, answer):
+    print("Test 11 passed")
+else:
+    print(f"Test 11 failed got {output} expected {answer}")
+
+#Test 12
+inputstring = "mississippi"
+inputpattern = "mis*is*ip*."
+output = solution.isMatch(inputstring, inputpattern)
+answer = True
+if Test(output, answer):
+    print("Test 12 passed")
+else:
+    print(f"Test 12 failed got {output} expected {answer}")
+
+#Test 13
+inputstring = "bbba"
+inputpattern = ".*a*a"
+output = solution.isMatch(inputstring, inputpattern)
+answer = True
+if Test(output, answer):
+    print("Test 13 passed")
+else:
+    print(f"Test 13 failed got {output} expected {answer}")
+
+#Test 14
+inputstring = "aaca"
+inputpattern = "ab*a*c*a"
+output = solution.isMatch(inputstring, inputpattern)
+answer = True
+if Test(output, answer):
+    print("Test 14 passed")
+else:
+    print(f"Test 14 failed got {output} expected {answer}")
+
+#Test 15
+inputstring = "aa"
+inputpattern = "a*aa"
+output = solution.isMatch(inputstring, inputpattern)
+answer = True
+if Test(output, answer):
+    print("Test 15 passed")
+else:
+    print(f"Test 15 failed got {output} expected {answer}")
+
+#Test 16
+inputstring = "a"
+inputpattern = ".*."
+output = solution.isMatch(inputstring, inputpattern)
+answer = True
+if Test(output, answer):
+    print("Test 16 passed")
+else:
+    print(f"Test 16 failed got {output} expected {answer}")
+"""
