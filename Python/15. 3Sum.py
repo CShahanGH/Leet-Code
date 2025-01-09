0d18621eb4f20a2a2f725a567814d76d8af7b793
Charlie <charlieshahan@gmail.com>
2025-01-09 15:19:35 -0600

    Started 15. 3Sum

---
+++ Python/15. 3Sum.py
@@ -0,0 +1,69 @@
+#Problem Requirements
+"""
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+Example 1:
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+
+Example 2:
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+
+Example 3:
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+ 
+
+Constraints:
+
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+"""
+
+
+#Design (in design folder 15.3Sum)
+"""
+
+"""
+
+#Learned
+"""
+
+"""
+
+#Code Start
+class Solution(object):
+    pass #Starting Code Here
+
+
+#Test Code
+def Test(id, output, answer):
+    try:
+        assert output == answer
+        print(f"Test {id} passed")
+    except:
+        print(f"Test {id} failed got {output} expected {answer}")
+
+
+solution = Solution()
+
+#Test 1
+input = None
+output = None
+answer = None
+
+Test (1, output, answer) 
+
+
+
