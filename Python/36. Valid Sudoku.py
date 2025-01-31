6d450cc7e5b2c18da048fa00c94e3cd0e893fb07
Charlie <charlieshahan@gmail.com>
2025-01-31 14:48:55 -0600

    Valid Sudoku finished

---
+++ Python/36. Valid Sudoku.py
@@ -0,0 +1,115 @@
+#Problem Requirements
+"""
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+Note:
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+
+Example 2:
+
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+Constraints:
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit 1-9 or '.'.
+"""
+
+
+#Design
+"""
+Hash_set because we don't want duplicates
+"""
+
+#Implementation Problems and Solutions
+"""
+I had a stupid way of doing the sub boxes, using a sub 2d list by dividing the row/3 and col/3 is a much simpler way LOL
+Also use a set for O(1) lookup instead of O(n) with list
+"""
+
+#Code Start
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        row_set = defaultdict(set)        
+        col_set = defaultdict(set)
+        box_set = defaultdict(set)
+
+        for row in range(9):
+            for col in range(9):
+
+                if board[row][col] == ".":
+                    continue
+
+                if board[row][col] in row_set[row] or board[row][col] in col_set[col] or board[row][col] in box_set[(row // 3, col//3 )]:
+                    return False
+
+                row_set[row].add(board[row][col])
+                col_set[col].add(board[row][col])
+                box_set[(row // 3, col//3 )].add(board[row][col])
+
+        
+        return True 
+            
+
+
+#Test Code - sometimes needs to change depending on the data type or constraints of output and answer
+def Test(id, output, answer):
+    try:
+        assert output == answer
+        print(f"Test {id} passed")
+    except:
+        print(f"Test {id} failed got {output} expected {answer}")
+
+
+solution = Solution()
+
+#Test 1
+input = [["5","3",".",".","7",".",".",".","."]
+        ,["6",".",".","1","9","5",".",".","."]
+        ,[".","9","8",".",".",".",".","6","."]
+        ,["8",".",".",".","6",".",".",".","3"]
+        ,["4",".",".","8",".","3",".",".","1"]
+        ,["7",".",".",".","2",".",".",".","6"]
+        ,[".","6",".",".",".",".","2","8","."]
+        ,[".",".",".","4","1","9",".",".","5"]
+        ,[".",".",".",".","8",".",".","7","9"]]
+output = solution.isValidSudoku(input)
+answer = True
+
+Test (1, output, answer) 
+
+
+
